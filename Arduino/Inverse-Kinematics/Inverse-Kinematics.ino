91692dc23a283d245fb60bb2af5a6a97554ed233
Danila Loginov <loginovdanila@gmail.com>
2017-09-11 17:50:13 +0300

    Inverse-Kinematics sketch added

---
+++ Arduino/Inverse-Kinematics/Inverse-Kinematics.ino
@@ -0,0 +1,115 @@
+#include <Servo.h>
+#include <meArm.h>
+
+#define PC_SERIAL_BAUDRATE  115200
+
+#define BASE_SERVO_PIN      9
+#define SHOULDER_SERVO_PIN  10
+#define ELBOW_SERVO_PIN     6
+#define GRIPPER_SERVO_PIN      11
+
+meArm arm(137, 47, -pi / 4, pi / 4,     // base
+          133, 39, pi / 4, 3 * pi / 4,  // shoulder
+          145, 94, 0, -pi / 4,          // elbow
+          80, 120, pi / 2, 0);          // gripper
+
+String inputBuffer = "";
+
+void setup()
+{
+    Serial.begin(PC_SERIAL_BAUDRATE);
+
+    arm.begin(BASE_SERVO_PIN, SHOULDER_SERVO_PIN, ELBOW_SERVO_PIN, GRIPPER_SERVO_PIN);
+
+    Serial.println("MeArm initialized");
+}
+
+void loop()
+{
+    String input = getInput();
+
+    if (input.length() > 0) {
+        if (isCoordinateCommand(input)) {
+            executeCoordinateCommand(input);
+        }
+        else if (input == "g") {
+            Serial.println("Opening the gripper");
+            arm.openGripper();
+        }
+        else if (input == "G") {
+            Serial.println("Closing the gripper");
+            arm.closeGripper();
+        }
+        else if (input == "O") {
+            Serial.println("Going to the home point");
+            arm.gotoPoint(0.0, 100.0, 50.0);
+        }
+    }
+}
+
+String getInput()
+{
+    String input = "";
+
+    if (Serial.available()) {
+        char c = Serial.read();
+
+        if (c == '\n') {
+            input = inputBuffer;
+            inputBuffer = "";
+        }
+        else {
+            inputBuffer += c;
+        }
+    }
+
+    return input;
+}
+
+boolean isCoordinateCommand(String command)
+{
+    return (command[0] == 'X' || command[0] == 'Y' || command[0] == 'Z');
+}
+
+void executeCoordinateCommand(String command)
+{
+    float x = arm.getX(),
+          y = arm.getY(),
+          z = arm.getZ();
+
+    // Get integer value from command string after the first character
+    int val = command.substring(1).toInt();
+
+    // Determine what coordinate will be changed
+    switch (command[0]) {
+        case 'X':
+            x = val;
+            break;
+        case 'Y':
+            y = val;
+            break;
+        case 'Z':
+            z = val;
+            break;
+        default:
+            return;
+    }
+
+    Serial.print("Going to the point (");
+    Serial.print(x);
+    Serial.print(", ");
+    Serial.print(y);
+    Serial.print(", ");
+    Serial.print(z);
+    Serial.print(")");
+
+    if (arm.isReachable(x, y, z)) {
+        Serial.println();
+    }
+    else {
+        Serial.println(", which is not reachable");
+    }
+
+    arm.gotoPoint(x, y, z);
+}
+
