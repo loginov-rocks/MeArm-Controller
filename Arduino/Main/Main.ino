5e346026d589d3e94b45dc51f94b92c13061f0eb
Danila Loginov <loginovdanila@gmail.com>
2017-09-12 17:34:00 +0300

    Gather servos and inverse kinematics sketches into one with mode switching

---
+++ Arduino/Main/Main.ino
@@ -0,0 +1,238 @@
+#include <Servo.h>
+#include <meArm.h>
+
+#define PC_SERIAL_BAUDRATE  115200
+
+#define BASE_SERVO_PIN      9
+#define SHOULDER_SERVO_PIN  10
+#define ELBOW_SERVO_PIN     6
+#define GRIPPER_SERVO_PIN   11
+
+Servo base,
+      shoulder,
+      elbow,
+      gripper;
+
+meArm arm(137, 47,  -pi / 4, pi / 4,     // base
+          133, 39,  pi / 4,  3 * pi / 4, // shoulder
+          145, 94,  0,       -pi / 4,    // elbow
+          80,  120, pi / 2,  0);         // gripper
+
+String inputBuffer = "";
+
+byte mode = 1;
+
+void setup()
+{
+    Serial.begin(PC_SERIAL_BAUDRATE);
+
+    switch (mode) {
+        case 2:
+            setupCoordinatesMode();
+            break;
+        default:
+            setupServosMode();
+            break;
+    }
+
+    Serial.println("MeArm initialized");
+}
+
+void loop()
+{
+    String input = getInput();
+
+    if (input.length() > 0) {
+        if (isServoCommand(input)) {
+            switchMode(1);
+            executeServoCommand(input);
+        }
+        else if (isCoordinateCommand(input)) {
+            switchMode(2);
+            executeCoordinateCommand(input);
+        }
+        else if (input == "c") {
+            switchMode(2);
+            Serial.println("Opening the gripper");
+            arm.openGripper();
+        }
+        else if (input == "C") {
+            switchMode(2);
+            Serial.println("Closing the gripper");
+            arm.closeGripper();
+        }
+    }
+}
+
+void setupServosMode()
+{
+    base.attach(BASE_SERVO_PIN);
+    shoulder.attach(SHOULDER_SERVO_PIN);
+    elbow.attach(ELBOW_SERVO_PIN);
+    gripper.attach(GRIPPER_SERVO_PIN);
+}
+
+void disarmServosMode()
+{
+    base.detach();
+    shoulder.detach();
+    elbow.detach();
+    gripper.detach();
+}
+
+void setupCoordinatesMode()
+{
+    arm.begin(BASE_SERVO_PIN, SHOULDER_SERVO_PIN, ELBOW_SERVO_PIN, GRIPPER_SERVO_PIN);
+}
+
+void disarmCoordinatesMode()
+{
+    arm.end();
+}
+
+boolean switchMode(byte targetMode)
+{
+    if (targetMode == mode) {
+        return false;
+    }
+
+    switch (mode) {
+        case 1:
+            disarmServosMode();
+            break;
+        case 2:
+            disarmCoordinatesMode();
+            break;
+        default:
+            return false;
+    }
+
+    switch (targetMode) {
+        case 1:
+            setupServosMode();
+            break;
+        case 2:
+            setupCoordinatesMode();
+            break;
+        default:
+            return false;
+    }
+
+    mode = targetMode;
+
+    return true;
+}
+
+String getInput()
+{
+    String input = "";
+
+    if (Serial.available()) {
+        char c = Serial.read();
+
+        if (c == '\n') {
+            input = inputBuffer;
+            inputBuffer = "";
+        }
+        else {
+            inputBuffer += c;
+        }
+    }
+
+    return input;
+}
+
+boolean isServoCommand(String command)
+{
+    return (command[0] == 'B' || command[0] == 'S' || command[0] == 'E' || command[0] == 'G');
+}
+
+boolean isCoordinateCommand(String command)
+{
+    return (command[0] == 'X' || command[0] == 'Y' || command[0] == 'Z');
+}
+
+void executeServoCommand(String command)
+{
+    Servo *servo;
+
+    // Determine what servo will be moved
+    switch (command[0]) {
+        case 'B':
+            servo = &base;
+            Serial.print("Base");
+            break;
+        case 'S':
+            servo = &shoulder;
+            Serial.print("Shoulder");
+            break;
+        case 'E':
+            servo = &elbow;
+            Serial.print("Elbow");
+            break;
+        case 'G':
+            servo = &gripper;
+            Serial.print("Gripper");
+            break;
+        default:
+            return;
+    }
+
+    // Get integer value from command string after the first character
+    int val = command.substring(1).toInt();
+
+    // Validate the value
+    if (val < 0 || val > 180) {
+        Serial.println(" is not rotating because of incorrect angle");
+        return;
+    }
+
+    Serial.print(" is rotating to ");
+    Serial.print(val);
+    Serial.println(" degrees");
+
+    servo->write(val);
+}
+
+void executeCoordinateCommand(String command)
+{
+    float x = arm.getX(),
+          y = arm.getY(),
+          z = arm.getZ();
+
+    // Get float value from command string after the first character
+    float val = command.substring(1).toFloat();
+
+    // Determine what coordinate will be changed
+    switch (command[0]) {
+        case 'X':
+            x = val;
+            break;
+        case 'Y':
+            y = val;
+            break;
+        case 'Z':
+            z = val;
+            break;
+        default:
+            return;
+    }
+
+    Serial.print("Going to the point (");
+    Serial.print(x);
+    Serial.print(", ");
+    Serial.print(y);
+    Serial.print(", ");
+    Serial.print(z);
+    Serial.print(")");
+
+    if (arm.isReachable(x, y, z)) {
+        Serial.println();
+    }
+    else {
+        Serial.println(", which is not reachable");
+    }
+
+    arm.gotoPoint(x, y, z);
+}
+
