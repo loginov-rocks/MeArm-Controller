6793769438660b77bb44ad9f9ec8fd203bc5249e
Danila Loginov <loginovdanila@gmail.com>
2017-09-13 03:00:59 +0300

    Bluetooth module config sketch added

---
+++ Arduino/Bluetooth-Config/Bluetooth-Config.ino
@@ -0,0 +1,35 @@
+/**
+ * Wire HC-05 bluetooth module to the Arduino Uno:
+ * RX  - 3
+ * TX  - 2
+ * GND - GND
+ * VCC - 5V
+ *
+ * If you use Arduino with additional UARTs, you can simply replace `SoftwareSerial`
+ * with the `Serial` you use (e.g. `Serial2` or `Serial3` for Arduino Mega).
+ */
+
+#include <SoftwareSerial.h>
+
+#define PC_SERIAL_BAUDRATE  38400
+#define BT_SERIAL_BAUDRATE  38400
+
+SoftwareSerial SerialBt(2, 3); // RX, TX
+
+void setup()
+{
+    Serial.begin(PC_SERIAL_BAUDRATE);
+    SerialBt.begin(BT_SERIAL_BAUDRATE);
+}
+
+void loop()
+{
+    if (SerialBt.available()) {
+        Serial.write(SerialBt.read());
+    }
+
+    if (Serial.available()) {
+        SerialBt.write(Serial.read());
+    }
+}
+
